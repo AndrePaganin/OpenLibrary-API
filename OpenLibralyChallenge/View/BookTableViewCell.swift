cca4400791fc1d528159db3623cb3831fa3ca9b9
Andre Paganin <andre.ramos_ext@getninjas.com.br>
2022-04-26 13:01:53 +0100

    create BookTableViewCell

---
+++ OpenLibralyChallenge/View/BookTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  BookTableViewCell.swift
+//  OpenLibralyChallenge
+//
+//  Created by Andre Paganin on 26/04/2022.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookImage: UIImageView!
+    @IBOutlet weak var bookTitle: UILabel!
+    @IBOutlet weak var bookAuthor: UILabel!
+    @IBOutlet weak var bookPubDate: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
